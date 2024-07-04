39d0e7b632e38fd3ec60cd6b2eabfbbf404ffaf2
Ali Tahmasebi <tahmasebi83@gmail.com>
2024-07-04 17:51:46 +0330

    subscribers_posts migration added

---
+++ app/Console/Commands/SendEmailsToSubscribers.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SendEmailsToSubscribers extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-emails';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send emails to subscribers';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        
+    }
+}
